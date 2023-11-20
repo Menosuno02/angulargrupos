110afbd7a84faf350fdf3de2cdb609dc9bdc11a4
Alejandro López Labajo <mainusone1@gmail.com>
2023-11-20 14:03:36 +0100

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { ServiceGrupos } from './services/service.grupos';
 import { FormsModule } from '@angular/forms';
 import { CreatealumnoComponent } from './components/createalumno/createalumno.component';
 import { appRoutingProvider, routing } from './app.routing';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
-  declarations: [AppComponent, GruposComponent, CreatealumnoComponent],
+  declarations: [AppComponent, GruposComponent, CreatealumnoComponent, MenuComponent],
   imports: [BrowserModule, HttpClientModule, FormsModule, routing],
   providers: [ServiceGrupos, appRoutingProvider],
   bootstrap: [AppComponent],
